254ec9887cc9cf899d3dbdb1ab382c45bc137413
josephemswiler <josephemswiler@gmail.com>
2018-07-29 19:34:40 -0500

    AppSync wip

--- react-aws-app/src/App.js
+++ react-aws-app/src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import './App.css'
 import { withAuthenticator } from 'aws-amplify-react'
-import { API, Analytics } from 'aws-amplify'
+import { API, Analytics, graphqlOperation } from 'aws-amplify'
 
 let apiName = 'PetAPI'
 let path = '/pets'
 
+const ListPets = `
+  query {
+    listPets {
+      items {
+        id
+        name
+      }
+    }
+  }
+`
+
+const CreatePet = `
+  mutation($name: String!) {
+    createPet(input: {
+      name: $name
+    }) {
+      id
+    }
+  }
+`
+
 class App extends Component {
   state = {
+    name: '',
     pets: []
   }
 
   async componentDidMount () {
-    const data = await API.get(apiName, path)
-    console.log('data: ', data)
-    this.setState({
-      pets: data.data
-    })
+    const pets = await API.graphql(graphqlOperation(ListPets))
+    console.log('pets: ', pets) // optional, if you would like to view the shape of the data
+    this.setState({ pets: pets.data.listPets.items })
   }
 
   addToCart = () => {
@@ -24,11 +44,24 @@ class App extends Component {
     Analytics.record('Item added to cart!')
   }
 
+  onChange = e => {
+    this.setState({ name: e.target.value })
+  }
+
+  createPet = () => {
+    const pet = { name: this.state.name }
+    API.graphql(graphqlOperation(CreatePet, pet))
+    const pets = [...this.state.pets, pet]
+    this.setState({ pets, name: '' })
+  }
+
   render () {
     return (
       <div className='App'>
-        {this.state.pets.map((pet, index) => <h2 key={index}>{pet}</h2>)}
-        <button onClick={this.addToCart}>Add To Cart</button>
+        {this.state.pets.map((pet, index) => <h2 key={index}>{pet.name}</h2>)}
+        <input onChange={this.onChange} placeholder='Pet name' />
+        <button onClick={this.createPet}>Create Pet</button>
+        {/* <button onClick={this.addToCart}>Add To Cart</button> */}
       </div>
     )
   }
